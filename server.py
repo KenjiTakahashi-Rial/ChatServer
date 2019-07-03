f2282a78dabed8be6dc64c211d654a3660edf0d9
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2019-07-03 16:54:38 -0700

    made initialize_user() in server.py and moved some code into it

--- server.py
+++ server.py
@@ -13,24 +13,6 @@ PORT = 1081
 # Use a header to know how long a message will be
 HEADER_LENGTH = 8
 
-
-def receive_data(client_socket):
-    """
-    Description:
-        Handles receiving data from a client
-    Arguments:
-        A client socket to receive data from
-    Return Value:
-        A dictionary with the header and message data
-    """
-
-    header = client_socket.recv(HEADER_LENGTH)
-    msg_len = int(header.decode('utf-8').strip())
-    message = client_socket.recv(msg_len)
-
-    return {'header': header, 'message': message}
-
-
 # Set up the server socket and start listening
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((IP_ADDRESS, PORT))
@@ -42,6 +24,46 @@ print(f"\nListening for connections on {IP_ADDRESS}:{PORT}...\n")
 # Keep track of sockets and client information
 sockets = [server_socket]
 clients = {}
+usernames = {}
+
+
+def initialize_user(client_socket):
+    """
+    Description:
+        Gets the client data and stores it in the server
+    Agruments:
+        A client socket to initialize
+    Return Value:
+        Returns the new client's username
+    """
+
+    user = receive_data(client_socket)
+
+    sockets.append(client_socket)
+    clients[client_socket] = user
+
+    username = user['message'].decode('utf-8')
+    usernames[username] = client_socket
+
+    return username
+
+
+def receive_data(client_socket):
+    """
+    Description:
+        Handles receiving data from a client
+    Arguments:
+        A client socket to receive data from
+    Return Value:
+        A dictionary with the encoded header and message data
+    """
+
+    header = client_socket.recv(HEADER_LENGTH)
+    msg_len = int(header.decode('utf-8').strip())
+    message = client_socket.recv(msg_len)
+
+    return {'header': header, 'message': message}
+
 
 # Main server loop
 while True:
@@ -54,13 +76,10 @@ while True:
         if ready_socket == server_socket:
             client_socket, client_address = server_socket.accept()
 
-            user = receive_data(client_socket)
+            new_user = initialize_user(client_socket)
 
-            sockets.append(client_socket)
-            clients[client_socket] = user
-
-            print(f"\nNew connection: {client_address[0]}:{client_address[1]}, ",
-                  f"Username: {user['message']}\n")
+            print(f"\nNew connection: {client_address[0]}:{client_address[1]}",
+                  f", Username: {new_user}\n")
 
         # Receive data and distribute message to clients
         else:
