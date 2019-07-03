ef35bcf0e1888431b784fbeb913ccd98d0593370
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2019-07-03 16:05:48 -0700

    made the main server loop

--- client.py
+++ client.py
@@ -36,8 +36,41 @@ server_socket.bind((IP_ADDRESS, PORT))
 server_socket.listen()
 
 # DEBUG
-print(f"Listening for connections on {IP_ADDRESS}:{PORT}...")
+print(f"\nListening for connections on {IP_ADDRESS}:{PORT}...\n")
 
 # Keep track of sockets and client information
 sockets = [server_socket]
 clients = {}
+
+# Main server loop
+while True:
+    # Get the ready sockets
+    read_sockets, write_sockets, error_sockets =
+    select.select(sockets, [], sockets)
+
+    for ready_socket in read_sockets:
+        # New connection
+        if ready_socket == server_socket:
+            client_socket, client_address = server_socket.accept()
+
+            user = receive_data(client_socket)
+
+            sockets.append(client_socket)
+            clients[client_socket] = user
+
+            print(f"\nNew connection: {client_address[0]}:{client_address[1]}, ",
+                  f"Username: {user['message']}\n")
+
+        # Receive data and distribute message to clients
+        else:
+            data = receive_data(ready_socket)
+            user = clients[ready_socket]
+
+            for client_socket in clients:
+                client_socket.send(user['header'] + user['message'] +
+                                   data['header'] + data['message'])
+
+    # Error handling
+    for ready_socket in error_sockets:
+        sockets.remove(ready_socket)
+        del clients[ready_socket]
