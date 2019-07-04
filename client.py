6eedaec96693421b86c7024dea5a0da542e22e36
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2019-07-04 12:59:30 -0700

    trying to separate messaging functions from server.py and client.py

--- client.py
+++ client.py
@@ -8,11 +8,14 @@ import errno
 import socket
 import sys
 
+from client_messaging import *
+
+
+# INITIAL SETUP ####################################################
+
+
 IP_ADDRESS = socket.gethostname()
 PORT = 1081
-
-# Use a header to know how long a message will be
-HEADER_LENGTH = 8
 
 # Set up the client and connect
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -27,68 +30,7 @@ except Exception as e:
 client_socket.setblocking(False)
 
 
-def send_message(message):
-    """
-    Description:
-        Handles sending a message to the server
-    Arguments:
-        A message to send
-    Return Value:
-        True if the message sent successfully
-        False if the message is blank or an error occurred
-    """
-
-    # Do not send blank messages
-    if len(message.strip()) == 0:
-        return False
-
-    try:
-        header = f"{len(message):<{HEADER_LENGTH}}"
-        client_socket.send((header + message).encode('utf-8'))
-
-        return True
-    except Exception as e:
-        connection_error()
-
-        return False
-
-
-def get_message():
-    """
-    Description:
-        Handles getting a message from the server
-    Arguments:
-        None
-    Return Value:
-        True if the message was received successfully
-        False if no messages to receive or an error occurred
-    """
-
-    try:
-        header = client_socket.recv(HEADER_LENGTH)
-
-        # If connection was terminated, there is no header
-        if len(header) == 0:
-            connection_error()
-
-        head_len = int(header.decode('utf-8').strip())
-        message = client_socket.recv(head_len).decode('utf-8')
-
-        return "<= " + message
-
-    # When there is no incoming data, EAGAIN and EWOULDBLOCK are thrown,
-    # so return False for those, otherwise print an error and exit
-    except IOError as e:
-        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            connection_error()
-
-        return False
-
-    # Any other errors catch here
-    except Exception as e:
-        connection_error()
-
-        return False
+# FUNCTIONS ######################################################
 
 
 def connection_error():
@@ -106,7 +48,9 @@ def connection_error():
     sys.exit()
 
 
-# Main client loop
+# MAIN CLIENT LOOP ##################################################
+
+
 while True:
     message = get_message()
 
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #########################################
-# Chat Server                           #
-# GungHo engineering test               #
+# server.py                             #
+# The chat server                       #
 # by Kenji Takahashi-Rial               #
 #########################################
 
@@ -8,18 +8,24 @@ import select
 import socket
 import sys
 
+from server_messaging import *
+
+
+# INITIAL SETUP #################################################
+
+
 IP_ADDRESS = socket.gethostname()
 PORT = 1081
 
-# Use a header to know how long a message will be
-HEADER_LENGTH = 8
-
 # Set up the server socket and start listening
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Allows reuse of address/port
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
 server_socket.bind((IP_ADDRESS, PORT))
 server_socket.listen()
 
-# DEBUG
 print(f"\nListening for connections on {IP_ADDRESS}:{PORT}...\n")
 
 # Keep track of sockets and client information
@@ -32,92 +38,7 @@ usernames = {}
 rooms = {"chat": [], "hottub": [], "PAD": [], "anime": []}
 
 
-def send_message(message, client_socket):
-    """
-    Description:
-        Handles sending a message to a client
-    Arguments:
-        A message to send
-        A client socket to send a message to
-    Return Value:
-        True if the message sent successfully
-        False if an error occurred
-    """
-
-    try:
-        header = f"{len(message):<{HEADER_LENGTH}}"
-        client_socket.send((header + message).encode('utf-8'))
-
-        return True
-
-    # Exceptions indicate ungraceful client termanation
-    except Exception as e:
-        connection_terminated(client_socket)
-
-        return False
-
-
-def get_message(client_socket):
-    """
-    Description:
-        Handles receiving a message from a client
-    Arguments:
-        A client socket to get a message from
-    Return Value:
-        True if the message was received successfully
-        False if a connection was terminated or an error occurred
-    """
-
-    try:
-        header = client_socket.recv(HEADER_LENGTH)
-
-        # If connection was terminated, there is no header
-        if len(header) == 0:
-            connection_terminated(client_socket)
-
-            return False
-
-        msg_len = int(header.decode('utf-8').strip())
-        message = client_socket.recv(msg_len).decode('utf-8')
-
-        return message
-
-    # Exceptions indicate ungraceful client termanation
-    except Exception as e:
-        connection_terminated(client_socket)
-
-        return False
-
-
-def server_command(command, client_socket):
-    """
-    Description:
-        When a user precedes a message with backslash, it is
-        interpreted as a command. This parses it.
-    Arguments:
-        A command to be executed
-    Return Value:
-        True if the command was carried out
-        False if there was an error
-    """
-
-    commands = {"rooms": rooms, "r": rooms,
-                "join": join, "j": join,
-                "leave": leave, "l": leave,
-                "exit": exit, "x": exit,
-                "quit": exit, "q": exit}
-
-    def rooms():
-        pass
-
-    def join():
-        pass
-
-    def leave():
-        pass
-
-    def exit():
-        connection_terminated(client_socket)
+# FUNCTIONS ######################################################
 
 
 def initialize_user(client_socket, client_address):
@@ -186,10 +107,12 @@ def connection_terminated(client_socket):
     else:
         print(f"\nConnection {str_address} terminated by unnamed client\n")
 
-    client_socket.close()
+    # client_socket.close()
 
 
-# Main server loop
+# MAIN SERVER LOOP ##################################################
+
+
 while True:
     # Get the ready sockets
     read_sockets, write_sockets, error_sockets = (
