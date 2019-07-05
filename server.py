867db517fc6b1ed79940e0dcd9533eff2ad54d48
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2019-07-04 18:59:39 -0700

    filled out server_commands() in server.py, but '/join' doesn't work

--- server.py
+++ server.py
@@ -15,10 +15,30 @@ class Server():
     def __init__(self, server_socket, rooms={}, header_length=8):
         self.socket = server_socket
         self.sockets = [server_socket]
+
+        # A dictionary with the client socket as the key
+        # and the client object as the value
         self.clients = {}
         self.usernames = []
+
+        # A dictionary with the clients of the room as the key
+        # and a list of users in the room as a value
         self.rooms = rooms
+
         self.header_length = header_length
+
+    def __str__(self):
+        server_socket_str = f"server socket: {self.socket}\n\n"
+        sockets_str = f"sockets: {self.sockets}\n\n"
+        clients_str = f"clients: {self.clients}\n\n"
+        rooms_str = f"rooms: {self.rooms}\n\n"
+        header_str = f"header length: {self.header_length}\n\n"
+
+        return (server_socket_str +
+                sockets_str +
+                clients_str +
+                rooms_str +
+                header_str)
 
     def send(self, message, client_socket):
         """
@@ -53,7 +73,8 @@ class Server():
             A Server object
             A client socket to receive a message from
         Return Value:
-            True if the message was received successfully
+            The message if it was received successfully
+            True if a command was run successfully
             False if a connection was terminated or an error occurred
         """
 
@@ -67,46 +88,104 @@ class Server():
                 return False
 
             msg_len = int(header.decode('utf-8').strip())
-            message = client_socket.recv(msg_len).decode('utf-8')
+            message = client_socket.recv(msg_len).decode('utf-8').strip()
+
+            # Reroute to command function
+            if message[0] == '/' and message[1] != '/':
+                return self.server_command(message, client_socket)
 
             return message
 
         # Exceptions indicate ungraceful client termanation
         except Exception as e:
             self.connection_terminated(client_socket)
-
             return False
 
-    def server_command(self, command, client_socket):
+    def server_command(self, input, client_socket):
         """
         Description:
             When a user precedes a message with backslash, it is
             interpreted as a command. This parses it.
         Arguments:
             A Server object
-            A command to be executed
+            A command to be executed (including backslash)
         Return Value:
             True if the command was carried out
-            False if there was an error
+            False if an error occurred
         """
 
-        commands = {"rooms": rooms, "r": rooms,
-                    "join": join, "j": join,
-                    "leave": leave, "l": leave,
-                    "exit": exit, "x": exit,
-                    "quit": exit, "q": exit}
+        # Get the client object
+        client = self.clients[client_socket]
 
-        def rooms():
-            pass
+        # Separate by whitespace to get arguments
+        separated = input.split()
+        cmd = separated[0]
+        args = separated[1:]
 
-        def join():
-            pass
+        # Begin all command cases
 
-        def leave():
-            pass
+        if cmd == "/rooms" or cmd == "/r":
+            self.send("Active rooms:", client_socket)
 
-        def exit():
-            connection_terminated(client_socket)
+            for room in self.rooms:
+                self.send(f" * {room} ({len(self.rooms[room])})", client_socket)
+
+            self.send("End active rooms", client_socket)
+
+            return True
+
+        elif cmd == "/join" or cmd == "/j":
+            if len(arg) > 1:
+                self.send(f"Too many arguments for /join (takes 1)",
+                          client_socket)
+
+                return True
+
+            # Add the username to the rooms dictionary
+            # and the room to the client object
+            self.rooms[args[0]].append(client.username)
+            client.room = args[0]
+
+            return True
+
+        elif cmd == "/leave" or cmd == "/l":
+            # Remove the username from the rooms dictionary
+            # and the room from the client object
+            if (client.room is not None):
+                self.rooms[client.room].remove(client.username)
+                client.room = None
+
+            # Client is not in a room
+            else:
+                self.send("You are not currently in a room", client_socket)
+
+            return True
+
+        elif (cmd == "/exit" or cmd == "/x"
+              or cmd == "/quit" or cmd == "/q"):
+            # Remove the client from any rooms
+            if client.room is not None:
+                leave()
+
+            # Then disconnect the client
+            self.send("Come again soon!", client_socket)
+            self.connection_terminated(client_socket)
+
+            return True
+
+        # Incorrect command entered, show all valid commands
+        else:
+            self.send("Valid commands:", client_socket)
+            self.send("{:<6} - See active rooms".format(" * /rooms"),
+                      client_socket)
+            self.send("{:<6} - Join a room".format(" * /join"),
+                      client_socket)
+            self.send("{:<6} - Leave your current room".format(" * /leave"),
+                      client_socket)
+            self.send("{:<6} - Close your chat client".format(" * /exit"),
+                      client_socket)
+            self.send(" * To use backslash without a command, type //",
+                      client_socket)
 
     def initialize_user(self, client_socket):
         """
